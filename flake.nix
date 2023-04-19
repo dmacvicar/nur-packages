ee08cb592ab3f9baf96f62b5305cd9bd22b67545
Duncan Mac-Vicar P <duncan@mac-vicar.eu>
2023-04-19 13:22:54 +0200

    remove emacs29 and add gcp enabled kubectl

--- flake.nix
+++ flake.nix
@@ -2,35 +2,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
-    emacs-src.url = "github:emacs-mirror/emacs/emacs-29";
-    emacs-src.flake = false;
-
   };
 
-  outputs = { self, nixpkgs, flake-utils, emacs-overlay, emacs-src,... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs {
             inherit system;
-            overlays = [
-              emacs-overlay.overlays.default
-              (final: prev: {
-                emacs29 = (prev.emacsGit.override {
-                  withPgtk = true;
-                  withX = false;
-                  withWebP = true;
-                  withGTK2 = false;
-                }).overrideAttrs(old: {
-                  name = "emacs29";
-                  version = "29.0-${emacs-src.shortRev}";
-                  src = emacs-src;
-                });
-              })];
           };
       in rec {
-
+        packages.google-cloud-sdk = pkgs.google-cloud-sdk.withExtraComponents ([ pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin ]);
         packages.AX8-Edit = pkgs.callPackage ./pkgs/AX8-Edit { };
-        packages.emacs29 = pkgs.emacs29;
       });
 }
